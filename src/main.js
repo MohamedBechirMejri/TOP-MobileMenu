86f2852f4c188da8726cdaeca62ffd092067433c
MohamedBechirMejri <mohamed.bechir.mejri.dev@gmail.com>
2021-08-16 12:32:06 +0100

    make toggle functional

--- src/main.js
+++ src/main.js
@@ -1,1 +1,5 @@
-import './main.scss'; 
+import './main.scss';
+
+document.querySelector('.navtoggle').addEventListener('click', () => {
+  document.querySelector('.navlinks-container').classList.toggle('hidden');
+});
